2a512819a08ab960c926da0935b1e8f62f6cfbae
s583231 <179284352@qq.com>
2018-12-10 16:29:40 +0800

    update

--- src/views/dynamic/index.vue
+++ src/views/dynamic/index.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="app-container">
-    <el-button type="primary" size="small">新增动态</el-button>
+    <el-button type="primary" size="small" @click="dialogVisible = true">新增动态</el-button>
     <el-table
-      v-loading="listLoading"
       :data="list"
       style="margin-top:10px;"
       element-loading-text="Loading"
@@ -34,6 +33,24 @@
         </template>
       </el-table-column>
     </el-table>
+
+    <el-dialog title="新增动态" :visible.sync="dialogVisible" width="30%">
+      <el-form ref="form" :model="form" label-width="80px">
+        <el-form-item label="标题">
+          <el-input v-model="form.name" placeholder="请输入标题"></el-input>
+        </el-form-item>
+        <el-form-item label="时间">
+          <el-date-picker v-model="time" type="date" placeholder="选择日期"></el-date-picker>
+        </el-form-item>
+        <el-form-item label="内容">
+          <el-input type="textarea" :rows="2" placeholder="请输入内容" v-model="form.content"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">确定</el-button>
+          <el-button @click="dialogVisible = false">取消</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 
@@ -42,6 +59,9 @@ export default {
   components: {},
   data() {
     return {
+      dialogVisible: false,
+      time: "",
+      form: {},
       list: [
         {
           content: "content",
@@ -63,7 +83,11 @@ export default {
   },
   created() {},
   mounted() {},
-  methods: {}
+  methods: {
+    onSubmit() {
+      this.dialogVisible = false;
+    }
+  }
 };
 </script>
 
--- src/views/partyActivity/index.vue
+++ src/views/partyActivity/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app-container">
-    <el-button type="primary" size="small">新增活动</el-button>
+    <el-button type="primary" size="small" @click="dialogVisible = true">新增活动</el-button>
     <el-table
       v-loading="listLoading"
       :data="list"
@@ -34,6 +34,23 @@
         </template>
       </el-table-column>
     </el-table>
+    <el-dialog :visible.sync="dialogVisible" title="新增活动" width="30%">
+      <el-form ref="form" :model="form" label-width="80px">
+        <el-form-item label="标题">
+          <el-input v-model="form.name" placeholder="请输入标题"></el-input>
+        </el-form-item>
+        <el-form-item label="时间">
+          <el-date-picker v-model="time" type="date" placeholder="选择日期"></el-date-picker>
+        </el-form-item>
+        <el-form-item label="内容">
+          <el-input v-model="form.content" :rows="2" type="textarea" placeholder="请输入内容"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">确定</el-button>
+          <el-button @click="dialogVisible = false">取消</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 
@@ -42,6 +59,9 @@ export default {
   components: {},
   data() {
     return {
+      dialogVisible: false,
+      time: "",
+      form: {},
       list: [
         {
           content: "content",
@@ -63,7 +83,11 @@ export default {
   },
   created() {},
   mounted() {},
-  methods: {}
+  methods: {
+    onSubmit() {
+      this.dialogVisible = false;
+    }
+  }
 };
 </script>
 
